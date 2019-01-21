561726f9df11b51457873abe4cf831c147312f51
captain <3024667543@qq.com>
2019-01-22 01:06:06 +0800

    fix https

--- yafhome/application/controllers/Index.php
+++ yafhome/application/controllers/Index.php
@@ -62,8 +62,69 @@ class IndexController extends Yaf_Controller_Abstract {
         $client_id      = $request->getPost('client_id','');
         $client_secret  = $request->getPost('client_secret','');
         $code           = $request->getPost('code','');
+        $tokenUrl = 'https://github.com/login/oauth/access_token';
+        $params = [
+    			'client_id' => $client_id,
+    			'client_secret' => $client_secret,
+    			'code' => $code,
+    		];
+    		$headers = [
+    			'Accept: application/json',
+    			'Content-Type: application/x-www-form-urlencoded',
+    		];
+	      $request = self::curl($tokenUrl,$params,$headers,true );
         header('Content-type: application/json; charset=utf-8');
-        exit('{"access_token":"0dfa9ef62b48c834383354976d826c2e5badcd9b","token_type":"bearer","scope":"public_repo"}');
+        exit($request);
+        //exit('{"access_token":"0dfa9ef62b48c834383354976d826c2e5badcd9b","token_type":"bearer","scope":"public_repo"}');
+    }
+
+      /**
+     * @param $url 请求网址
+     * @param bool $params 请求参数
+     * @param int $ispost 请求方式
+     * @param int $https https协议
+     * @return bool|mixed
+     */
+    public static function curl($url, $params = false,$headers = array(), $ispost = 0, $https = 0) {
+        $httpInfo = array();
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
+        curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.118 Safari/537.36');
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        if ($https) {
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE); // 对认证证书来源的检查
+            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE); // 从证书中检查SSL加密算法是否存在
+        }
+        if($headers) {
+          curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        }
+        if ($ispost) {
+            curl_setopt($ch, CURLOPT_POST, true);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $params);
+            curl_setopt($ch, CURLOPT_URL, $url);
+        } else {
+            if ($params) {
+                if (is_array($params)) {
+                    $params = http_build_query($params);
+                }
+                curl_setopt($ch, CURLOPT_URL, $url . '?' . $params);
+            } else {
+                curl_setopt($ch, CURLOPT_URL, $url);
+            }
+        }
+
+        $response = curl_exec($ch);
+
+        if ($response === FALSE) {
+            //echo "cURL Error: " . curl_error($ch);
+            return false;
+        }
+        $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        $httpInfo = array_merge($httpInfo, curl_getinfo($ch));
+        curl_close($ch);
+        return $response;
     }
 
 
